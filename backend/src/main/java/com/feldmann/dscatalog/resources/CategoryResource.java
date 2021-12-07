7bb9ad9115c7f8204fa551b9ec8d86e3ca82cce9
evairlf <evairleitefeldmann@gmail.com>
2021-12-07 11:52:23 -0300

    added exception Handler and findCategoryById

--- backend/src/main/java/com/feldmann/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/feldmann/dscatalog/resources/CategoryResource.java
@@ -7,7 +7,9 @@ import com.feldmann.dscatalog.services.CategoryService;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,6 +27,12 @@ public class CategoryResource {
     public ResponseEntity<List<CategoryDTO>> findAll(){
         List<CategoryDTO> list = categoryService.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
+        CategoryDTO dto = categoryService.findById(id);
+        return ResponseEntity.ok().body(dto);
     }
 
 }
--- backend/src/main/java/com/feldmann/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/feldmann/dscatalog/services/CategoryService.java
@@ -1,11 +1,14 @@
 package com.feldmann.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
+
 
 import com.feldmann.dscatalog.dto.CategoryDTO;
 import com.feldmann.dscatalog.entities.Category;
 import com.feldmann.dscatalog.repositories.CategoryRepository;
+import com.feldmann.dscatalog.services.exceptions.EntityNotFoundException;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -30,4 +33,13 @@ public class CategoryService {
       return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
        
     }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not Found"));
+        return new CategoryDTO(entity);
+    }
+
+    
 }
