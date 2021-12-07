3199b2e65b3a8c51aed67fef57ef5fa0a8d5053d
evairlf <evairleitefeldmann@gmail.com>
2021-12-07 13:01:11 -0300

    Update Category

--- backend/src/main/java/com/feldmann/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/feldmann/dscatalog/resources/CategoryResource.java
@@ -10,9 +10,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -39,7 +39,7 @@ public class CategoryResource {
         return ResponseEntity.ok().body(dto);
     }
 
-    @PostMapping()
+    @PostMapping
     public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
         dto = categoryService.insert(dto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
@@ -48,4 +48,9 @@ public class CategoryResource {
         return ResponseEntity.created(uri).body(dto);
     }
 
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<CategoryDTO> update(@PathVariable Long id, @RequestBody CategoryDTO dto){
+        dto = categoryService.update(id ,dto);
+        return ResponseEntity.ok().body(dto);
+    }
 }
--- backend/src/main/java/com/feldmann/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/feldmann/dscatalog/services/CategoryService.java
@@ -4,40 +4,41 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
 
 import com.feldmann.dscatalog.dto.CategoryDTO;
 import com.feldmann.dscatalog.entities.Category;
 import com.feldmann.dscatalog.repositories.CategoryRepository;
-import com.feldmann.dscatalog.services.exceptions.EntityNotFoundException;
+import com.feldmann.dscatalog.services.exceptions.ResourceNotFoundException;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CategoryService {
-    
-   
+
     private CategoryRepository repository;
 
-
-    public CategoryService(CategoryRepository categoryRepository){
+    public CategoryService(CategoryRepository categoryRepository) {
         this.repository = categoryRepository;
     }
 
     @Transactional(readOnly = true)
-    public List<CategoryDTO> findAll(){
-      List<Category> list = repository.findAll();
-      //Stream() = transforma para stream para poder usar funções de alta ordem
-      //map() = mapeia o objeto e para cada valor do index x ele faz algo com a arrow function que no java é -> e nao =>
-      //Collect() = Desfaz a transformação de stream e volta para a forma desejada no caso CollectorstoList()
-      return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
-       
+    public List<CategoryDTO> findAll() {
+        List<Category> list = repository.findAll();
+        // Stream() = transforma para stream para poder usar funções de alta ordem
+        // map() = mapeia o objeto e para cada valor do index x ele faz algo com a arrow
+        // function que no java é -> e nao =>
+        // Collect() = Desfaz a transformação de stream e volta para a forma desejada no
+        // caso CollectorstoList()
+        return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+
     }
 
     @Transactional(readOnly = true)
     public CategoryDTO findById(Long id) {
         Optional<Category> obj = repository.findById(id);
-        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not Found"));
+        Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not Found"));
         return new CategoryDTO(entity);
     }
 
@@ -49,5 +50,15 @@ public class CategoryService {
         return new CategoryDTO(entity);
     }
 
-    
+    @Transactional
+    public CategoryDTO update(Long id, CategoryDTO dto) {
+        try {
+            Category entity = repository.getById(id);
+            entity.setName(dto.getName());
+            entity = repository.save(entity);
+            return new CategoryDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("id Not Found"+id);
+        }
+    }
 }
