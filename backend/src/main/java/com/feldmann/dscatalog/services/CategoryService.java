9116deb82eafc2348c696f0e588f376e936e6577
evairlf <evairleitefeldmann@gmail.com>
2021-12-07 10:48:09 -0300

    added seed in test api

--- backend/src/main/java/com/feldmann/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/feldmann/dscatalog/services/CategoryService.java
@@ -6,6 +6,7 @@ import com.feldmann.dscatalog.entities.Category;
 import com.feldmann.dscatalog.repositories.CategoryRepository;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CategoryService {
@@ -18,6 +19,7 @@ public class CategoryService {
         this.repository = categoryRepository;
     }
 
+    @Transactional(readOnly = true)
     public List<Category> findAll(){
         return repository.findAll();
     }
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_category (name) VALUES ('Books');
+INSERT INTO tb_category (name) VALUES ('Electronics');
+INSERT INTO tb_category (name) VALUES ('Computers');
+INSERT INTO tb_category (name) VALUES ('Desk');
+INSERT INTO tb_category (name) VALUES ('Office');
